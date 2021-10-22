36378f975bc4025a8d7b7b73906ba96552e32210
Alexis <alex10o@hotmail.com>
2021-10-22 04:18:42 -0300

    jugando a hacer el listado : )

--- src/component/List.js
+++ src/component/List.js
@@ -4,6 +4,47 @@ export default function List() {
     return (
         <div>
             <h1>Listado</h1>
+            <main className="contenido">
+    <div class="col-12">
+        <h2>Productos</h2>
+        <div class="alert">
+        </div>
+        <a class="btn btn-success mb-2">Agregar</a>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Código</th>
+                    <th>Precio</th>
+                    <th>Existencia</th>
+                    <th>Editar</th>
+                    <th>Eliminar</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><h4>producto.nombre</h4></td>
+                    <td><h4>producto.codigo</h4></td>
+                    <td><h4>producto.precio</h4></td>
+                    <td><h4>producto.existencia</h4></td>
+                    <td>
+                        <a class="btn btn-warning" >Editar <i
+                                class="fa fa-edit"></i></a>
+                    </td>
+                    <td>
+                        <form >
+                            <input type="hidden" name="id" />
+                            <button type="submit" class="btn btn-danger">Eliminar <i class="fa fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</main>
         </div>
     )
 }
