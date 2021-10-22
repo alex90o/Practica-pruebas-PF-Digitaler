558f84f47d6de7b6edead95db1982f212f0ca100
Alexis <alex10o@hotmail.com>
2021-10-22 02:26:50 -0300

    viendo como es el codigo de barra

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,21 @@
 export default function Home(){
     return (
            
-        <div>
+        <div className="col-12">
             <h1>Venta</h1>
-            <p> 
-            Lorem ipsum dolor sit amet amet conmet conmet   
-            </p> 
+            <form>
+            <div class="form-group">
+                <label for="codigo">Código de barras</label>
+                <input autofocus autocomplete="off" id="codigo"
+                       placeholder="Escanea el código o escríbelo y presiona Enter"
+                       type="text"
+                       class="form-control">
+                </input>
+                <div class="invalid-feedback"></div>
+
+            </div>
+        </form>
+            
         </div>
         
     )
